a75c213e4f5d27e0cfea28e148a2335c7382f68e
Shoukry <shokrysuleiman032@gmail.com>
2021-04-19 14:22:04 +0200

    feat(Filter): :sparkles: Filter By Type And Date Published
    User Can Filter Results By Type Or/And Date Published Throw Two Dropdowns In Search Page

--- src/@resources/services/YoutubeService.ts
+++ src/@resources/services/YoutubeService.ts
@@ -3,10 +3,13 @@ import http from './BaseHttpService';
 import { API_KEY } from '../config/config'
 class YoutubeService {
 
-	search(part: string, order: string, searchKey: string, pageToken: string, totalItemsPerPage: number) {
-		return http.get(`search?part=${part}&order=${order}
+	search(part: string, searchKey: string,
+		   pageToken: string, type: string,
+		   time:any,totalItemsPerPage: number) {
+		return http.get(`search?part=${part}
 							&q=${searchKey}&pageToken=${pageToken}
-							&maxResults=${totalItemsPerPage}&key=${API_KEY}`)
+							&maxResults=${totalItemsPerPage}&type=${type}
+							&publishedAfter=${time}&key=${API_KEY}`)
 	}
 
 };
--- src/views/YoutubeSearch.vue
+++ src/views/YoutubeSearch.vue
@@ -1,72 +1,136 @@
 <template>
-<div>
-  <!-- <select v-model="selected">
-  <option disabled value="">Please select one</option>
-  <option>A</option>
-  <option>B</option>
-  <option>C</option>
-</select> -->
-<!-- <select v-model="selected">
-  <option disabled value="">Please select one</option>
-  <option>A</option>
-  <option>B</option>
-  <option>C</option>
-</select> -->
-</div>
   <div>
-      <div  v-for="result in results" v-bind:key="result.id">
-        <template v-if="result.id.videoId">
-            <AppVideo :title="result.snippet.title" :description="result.snippet.description" :thumbnails="result.snippet.thumbnails" :videoId="result.id.videoId"/>
-        </template>
-         <template v-if="result.id.playlistId">
-            <AppPlayList :title="result.snippet.title" :description="result.snippet.description" :thumbnails="result.snippet.thumbnails" :videoId="result.id.videoId"/>
-        </template>
-        <template v-if="result.id.channelId">
-            <AppPlayList :title="result.snippet.title" :description="result.snippet.description" :thumbnails="result.snippet.thumbnails" :videoId="result.id.videoId"/>
-        </template>
-        
-      </div>
+    <select @change="onTypeChange($event)">
+      <option selected value="">All</option>
+      <option value="channel">Channel</option>
+      <option value="playlist">Playlist</option>
+    </select>
+    <select @change="onUploadDateChange($event)">
+      <option selected value="">Any time</option>
+      <option value="today">Today</option>
+      <option value="week">This week</option>
+      <option value="month">This month</option>
+    </select>
+  </div>
+  <div>
+    <div v-for="result in results" v-bind:key="result.id">
+      <template v-if="result.id.videoId">
+        <AppVideo
+          :title="result.snippet.title"
+          :description="result.snippet.description"
+          :thumbnails="result.snippet.thumbnails"
+          :videoId="result.id.videoId"
+        />
+      </template>
+      <template v-if="result.id.playlistId">
+        <AppPlayList
+          :title="result.snippet.title"
+          :description="result.snippet.description"
+          :thumbnails="result.snippet.thumbnails"
+          :videoId="result.id.videoId"
+        />
+      </template>
+      <template v-if="result.id.channelId">
+        <AppPlayList
+          :title="result.snippet.title"
+          :description="result.snippet.description"
+          :thumbnails="result.snippet.thumbnails"
+          :videoId="result.id.videoId"
+        />
+      </template>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import   YoutubeService  from '../@resources/services/YoutubeService';
-import AppVideo from "../components/AppVideo.vue"
-import AppPlayList from "../components/AppPlayList.vue"
+import { Options, Vue } from "vue-class-component";
+import YoutubeService from "../@resources/services/YoutubeService";
+import AppVideo from "../components/AppVideo.vue";
+import AppPlayList from "../components/AppPlayList.vue";
 
 @Options({
   components: {
     AppVideo,
-    AppPlayList
+    AppPlayList,
   },
-  provide:{
-    YoutubeService
-  }
-
+  provide: {
+    YoutubeService,
+  },
 })
 export default class YoutubeSearch extends Vue {
-
-  keySearch!:string;
-  results:any=[];
-  pageToken:string='';
- 
+  keySearch!: string;
+  results: any = [];
+  pageToken: string = "";
+  type: string = "";
+  time: any = new Date(0).toISOString();
+  readonly itemsPerPage = 5;
   created() {
-    if(this.$route.query.query){
+    if (this.$route.query.query) {
       this.keySearch = String(this.$route.query.query);
       this.search();
     }
   }
- 
+
   search() {
-    let keySearch = this.keySearch.split(' ').join('+')
-    YoutubeService.search(`snippet`,`viewCount`,keySearch,this.pageToken,5).then(res =>{
-      this.results = res['data']['items'];
-      console.log('res',res['data']['items'])
-    })
+      console.log('clicked')
+    this.getResults();
   }
 
+  onUploadDateChange(event: any) {
+      console.log('here')
+    switch (event.target.value) {
+      case "today":
+        this.time = new Date().toISOString();
+        break;
+      case "week":
+        this.time = this.getWeek();
+        break;
+      case "month":
+        this.time = this.getMonth();
+        break;
+      default:
+        this.time = new Date(0).toISOString();
+    }
+     console.log('here,eswer')
+    this.getResults();
+  }
 
-  
+  onTypeChange(event: any) {
+    this.type = event.target.value;
+    this.getResults();
+  }
+
+  getResults() {
+    YoutubeService.search(
+      `snippet`,
+      this.keySearchValue(),
+      this.pageToken,
+      this.type,
+      this.time,
+      this.itemsPerPage
+    ).then((res) => {
+      this.results = res["data"]["items"];
+    });
+  }
+
+  keySearchValue() {
+    return this.keySearch.split(" ").join("+");
+  }
+
+  getWeek() {
+    let day = new Date().getDay();
+    let diff = new Date().getDate() - day + (day == 0 ? -6 : 1);
+    console.log('new Date(new Date().setDate(diff))',new Date(new Date().setDate(diff)))
+    console.log('new Date(new Date().setDate(diff))',new Date(new Date().setDate(diff)).toISOString())
+    return new Date(new Date().setDate(diff)).toISOString();
+  }
+
+  getMonth() {
+    let date = new Date();
+    console.log('new Date(date.getFullYear(), date.getMonth(), 1)',new Date(date.getFullYear(), date.getMonth(), 1))
+        console.log('new Date(date.getFullYear(), date.getMonth(), 1)',new Date(date.getFullYear(), date.getMonth(), 1).toISOString())
+
+    return new Date(date.getFullYear(), date.getMonth(), 2).toISOString();
+  }
 }
 </script>
